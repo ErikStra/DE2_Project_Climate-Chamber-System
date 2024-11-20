9679f7f560d2b64ea8d5f80eb94bc5495823da55
Goosis <167889601+Goosis@users.noreply.github.com>
2024-11-20 13:51:05 +0100

    Update fan+senzor.c

--- src/fan+senzor.c
+++ src/fan+senzor.c
@@ -3,28 +3,28 @@
 #include <uart.h>
 #include <stdlib.h>
 
-#define DHT_PIN PD2  // Подключите DHT-сенсор к пину PD2
-#define FAN_PIN PB1  // Подключите вентилятор к пину D9 (PB1)
+#define DHT_PIN PD2  // Connect the DHT sensor to pin PD2
+#define FAN_PIN PB1  // Connect the fan to pin D9 (PB1)
 
-// -- Глобальные переменные --
-volatile uint8_t dht_data[5];  // Массив для хранения данных от DHT
+// -- Global Variables --
+volatile uint8_t dht_data[5];  // Array to store data from DHT sensor
 
-// -- Функции --
+// -- Functions --
 void dht_request(void) {
-    DDRD |= (1 << DHT_PIN);  // Установить DHT_PIN как выход
-    PORTD &= ~(1 << DHT_PIN); // Низкий уровень на 20 мс
+    DDRD |= (1 << DHT_PIN);  // Set DHT_PIN as output
+    PORTD &= ~(1 << DHT_PIN); // Low level for 20 ms
     _delay_ms(20);
-    PORTD |= (1 << DHT_PIN);  // Высокий уровень на 20-40 мкс
+    PORTD |= (1 << DHT_PIN);  // High level for 20-40 µs
     _delay_us(40);
-    DDRD &= ~(1 << DHT_PIN);  // Установить DHT_PIN как вход
+    DDRD &= ~(1 << DHT_PIN);  // Set DHT_PIN as input
 }
 
 uint8_t dht_response(void) {
     uint8_t response = 0;
     _delay_us(40);
-    if (!(PIND & (1 << DHT_PIN))) {  // Ожидание низкого уровня
+    if (!(PIND & (1 << DHT_PIN))) {  // Wait for low level
         _delay_us(80);
-        if (PIND & (1 << DHT_PIN)) {  // Ожидание высокого уровня
+        if (PIND & (1 << DHT_PIN)) {  // Wait for high level
             response = 1;
         }
         _delay_us(40);
@@ -35,12 +35,12 @@ uint8_t dht_response(void) {
 uint8_t dht_read_byte(void) {
     uint8_t byte = 0;
     for (uint8_t i = 0; i < 8; i++) {
-        while (!(PIND & (1 << DHT_PIN)));  // Ожидание высокого уровня
+        while (!(PIND & (1 << DHT_PIN)));  // Wait for high level
         _delay_us(30);
         if (PIND & (1 << DHT_PIN)) {
-            byte |= (1 << (7 - i));  // Если высокий уровень, записать 1
+            byte |= (1 << (7 - i));  // If high level, write 1
         }
-        while (PIND & (1 << DHT_PIN));  // Ожидание окончания бита
+        while (PIND & (1 << DHT_PIN));  // Wait for end of bit
     }
     return byte;
 }
@@ -62,13 +62,13 @@ void dht_read_data(void) {
 
 void fan_control(uint8_t temperature) {
     if (temperature >= 30) {
-        OCR1A = 255;  // Установить ШИМ на 100%
+        OCR1A = 255;  // Set PWM to 100%
         uart_puts("Fan at 100%\r\n");
     } else if (temperature >= 25) {
-        OCR1A = 128;  // Установить ШИМ на 50%
+        OCR1A = 128;  // Set PWM to 50%
         uart_puts("Fan at 50%\r\n");
     } else {
-        OCR1A = 0;  // Вентилятор выключен
+        OCR1A = 0;  // Turn off the fan
         uart_puts("Fan OFF\r\n");
     }
 }
@@ -78,21 +78,21 @@ int main(void) {
     uart_init(UART_BAUD_SELECT(115200, F_CPU));
     sei();
 
-    // Настройка FAN_PIN как выход и ШИМ
+    // Set FAN_PIN as output and PWM
     DDRB |= (1 << FAN_PIN);
     
-    // Настройка таймера для ШИМ на пине FAN_PIN (PB1/OC1A)
-    TCCR1A |= (1 << COM1A1) | (1 << WGM11);  // ШИМ, быстрый режим
-    TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS10);  // Без предделителя
-    ICR1 = 255;  // Верхнее значение для 8-битного ШИМ
+    // Set timer for PWM on FAN_PIN (PB1/OC1A)
+    TCCR1A |= (1 << COM1A1) | (1 << WGM11);  // PWM, fast mode
+    TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS10);  // No prescaler
+    ICR1 = 255;  // Top value for 8-bit PWM
 
     while (1) {
         dht_read_data();
 
-        uint8_t temp_int = dht_data[2];  // Целая часть температуры
-        uint8_t temp_dec = dht_data[3];  // Дробная часть температуры
+        uint8_t temp_int = dht_data[2];  // Integer part of temperature
+        uint8_t temp_dec = dht_data[3];  // Decimal part of temperature
 
-        // Отправить данные температуры через UART
+        // Send temperature data via UART
         uart_puts("Raw Temperature: ");
         itoa(temp_int, buffer, 10);
         uart_puts(buffer);
@@ -101,10 +101,10 @@ int main(void) {
         uart_puts(buffer);
         uart_puts(" °C\r\n");
 
-        // Управление вентилятором
+        // Control fan
         fan_control(temp_int);
 
-        _delay_ms(2000);  // Задержка между чтениями
+        _delay_ms(2000);  // Delay between readings
     }
 
     return 0;
