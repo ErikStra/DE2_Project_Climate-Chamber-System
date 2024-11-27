cd16aa6182200382d612f7da9703d29c64aad603
VasenkaV <166806420+VasenkaV@users.noreply.github.com>
2024-11-27 11:03:24 +0100

    Create rtc_control.h

---
+++ lib/RTC/rtc_control.h
@@ -0,0 +1,30 @@
+
+#ifndef __RTC_CONTROL_H__
+#define __RTC_CONTROL_H__
+  /**
+   * @desc   Voltmeter
+   *
+   * @param  void
+   *
+   * @return void
+   
+   */
+#include <avr/io.h>
+#include <stdlib.h>
+
+  int rtc_control (void);
+
+  void compare_time_with_sun(uint8_t current_hours, uint8_t current_minutes);
+
+  void rtc_read_time(void);
+
+  void load_settings_from_eeprom(void);
+
+  void save_settings_to_eeprom(void);
+
+  uint8_t bcdToDec(uint8_t val);
+
+  uint8_t decToBcd(uint8_t val);
+
+
+#endif
