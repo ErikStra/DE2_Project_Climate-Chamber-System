f673512e9c4f1c1845486146ebe3134dff971f6b
Goosis <167889601+Goosis@users.noreply.github.com>
2024-12-07 11:45:58 +0100

    Update fan_PID.h
    updeted fan_PID.h according to fan_PID.c

--- lib/fan_senzor/fan_PID.h
+++ lib/fan_senzor/fan_PID.h
@@ -2,7 +2,7 @@
  * ---------------------------------------------------+ 
  * @desc        User Interface Library h
  * ---------------------------------------------------+ 
- * @copyright   Copyright (C) 2024 Straka, Kostenko.
+ * @copyright   Copyright (C) 2024  Kostenko
  * @email       
  * @author      
  * @datum       25.11.2024
@@ -11,24 +11,49 @@
  * @tested      Arduino nano Atmega 328p
  * ---------------------------------------------------+
  */
-#include <stdio.h>          // Standardní knihovna
+#include <stdio.h>          // Standard library
 
 /** @definition */
 #ifndef __FAN_PID_H__
 #define __FAN_PID_H__
 
   /**
-   * @desc   Voltmeter
+   * @desc   PID control function
+   *
+   * @param  target_temp Target temperature
+   * @param  current_temp Current temperature
+   * @param  prev_error Pointer to the previous error value
+   * @param  integral Pointer to the integral value
+   *
+   * @return uint8_t Calculated PWM duty cycle
+   */
+  uint8_t pid_control(float target_temp, float current_temp, float *prev_error, float *integral);
+
+  /**
+   * @desc   Controls the fans using PID
    *
    * @param  void
    *
    * @return void
    */
+  void fan_control_pid(void);
 
-  uint8_t pid_control(float target_temp, float current_temp, float *prev_error, float *integral);
- void fan_control_pid(void);
- int fan_PID_init(void);
- int fan_PID_loop(void);
+  /**
+   * @desc   Initializes the fan PID system
+   *
+   * @param  void
+   *
+   * @return int Initialization status (0 for success)
+   */
+  int fan_PID_init(void);
 
+  /**
+   * @desc   Main loop for the fan PID system
+   *
+   * @param  void
+   *
+   * @return int Loop execution status (0 for success)
+   */
+  int fan_PID_loop(void);
 
 #endif
