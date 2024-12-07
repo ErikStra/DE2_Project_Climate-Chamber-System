169b83ed60537e2acf6a4792e7f062065c813db8
Goosis <167889601+Goosis@users.noreply.github.com>
2024-12-07 12:45:00 +0100

    created GPIO control

---
+++ lib/outputControl/outputControl.h
@@ -0,0 +1,16 @@
+#ifndef __OUTPUT_CONTROL_H__
+#define __OUTPUT_CONTROL_H__
+  /**
+   * @desc   smth
+   *
+   * @param  void
+   *
+   * @return void
+   
+   */
+#include <avr/io.h>
+#include <stdlib.h>
+
+  void outputControl_init(void);
+  void outputControl_loop(void);
+#endif
