97c0362dac0b12fc69e18e0fd0fbe15f8ca48f37
ZobacJ <246975@vutbr.cz>
2024-12-09 14:04:44 +0100

    smth

--- src/main.c
+++ src/main.c
@@ -89,7 +89,7 @@ while (1) { // Nekonečná smyčka
       fan_PID_loop();
 
       char buffer[100];
-      snprintf(buffer, sizeof(buffer), "KP1 = %d KI1 = %d, (int) KP1*10);
+      snprintf(buffer, sizeof(buffer), "KP1 = %d KI1 = %d KD1 = %d", (int) KP1*10, (int) KI1*10, (int) KD1*10);
       uart_puts(buffer);
       uart_puts("   \n\r");
 
